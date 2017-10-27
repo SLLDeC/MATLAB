dff2acfbf99b53fccb6bebd1e979680ea02a5314
SLLDeC <sabrina.lopez@live.com.ar>
2017-10-27 11:33:18 -0300

    detecta si los datos de ingka estan vacios

--- save_bad_data_ALTURAS.m
+++ save_bad_data_ALTURAS.m
@@ -1,4 +1,4 @@
-function [ o,r,bad ] = save_bad_data_ALTURAS(trial,step,r,o,stim,resp,asyn,error,t_acc,x_acc,y_acc,z_acc,t_pr,pr,mech_size,mech_bip,bad,B_laps )
+function [ o,r,bad ] = save_bad_data_ALTURAS(exp,trial,step,r,o,stim,resp,asyn,error,t_acc,x_acc,y_acc,z_acc,t_pr,pr,mech_size,mech_bip,bad,B_laps )
 
 bad(r).order = o;
 bad(r).trial = r;
@@ -16,6 +16,7 @@ bad(r).stim = stim;
 bad(r).resp = resp;
 bad(r).asyn = asyn;
 bad(r).type = error;
+if isempty(x_acc)==0
 bad(r).x_acc(1,:) = x_acc;
 bad(r).x_acc(2,:) = t_acc;
 bad(r).y_acc(1,:) = y_acc;
@@ -24,14 +25,23 @@ bad(r).z_acc(1,:) = z_acc;
 bad(r).z_acc(2,:) = t_acc;
 bad(r).pr(1,:) = pr;
 bad(r).pr(2,:) = t_pr;
+end
 
 o=o+1;
 r=r+1;
 
+
 if step==1
-     save('temporal_ent.mat','trial','bad')
+    save('temporal_ent.mat','trial','bad')
 elseif step==2
+    if strcmp('alturas',exp)==1
+        save('temporal_exp.mat','trial','bad')
+    elseif strcmp('calibracion',exp)==1
+        save('temporal_cal.mat','trial','bad')
+    end
+elseif step==3
     save('temporal_exp.mat','trial','bad')
+end
 
 end
 
